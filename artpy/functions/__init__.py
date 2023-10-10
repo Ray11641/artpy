a855dbfaca916cce0d36071206842681e73bda1c
ry222 <ry222@ece-acil08.managed.mst.edu>
2023-10-10 15:41:52 -0500

    Resuming work on ARTPY

--- artpy/functions/__init__.py
+++ artpy/functions/__init__.py
@@ -1,7 +1,9 @@
 """
-    SalientART PY: A Python library of Salient ART. This file is part of Neurol-
-    ogy phenotyping.
+    ARTPY: A Python library of Adaptive Resonance Theory based learning
+     models.
 """
 
 from .complementcoding import *
 from .generateclustercolors import *
+from .distance import *
+from .computermax import *
